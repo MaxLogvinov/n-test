c03dae37dd7555b4f581d79e3e0d049f56bc1230
Max Logvinov <maxlogvinov93@gmail.com>
2024-09-02 21:23:05 +0300

    add repository details

--- src/components/RepositoryDetails/RepositoryDetails.tsx
+++ src/components/RepositoryDetails/RepositoryDetails.tsx
@@ -1,5 +1,34 @@
 import styles from './RepositoryDetails.module.scss';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export default function RepositoryDetails() {
-  return <></>;
+export default function RepositoryDetails({ repo }) {
+  console.log(repo);
+  return (
+    <>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 32 }}>
+            {repo.name}
+          </Typography>
+          <Typography variant="h5" component="div">
+            Описание: {repo.description || 'Нет описания'}
+          </Typography>
+
+          {repo.license ? (
+            <>
+              <Typography variant="body2">{repo.license.name}</Typography>
+              <Typography variant="body2">{repo.license.key}</Typography>
+              <Typography variant="body2">{repo.license.spdx_id}</Typography>
+            </>
+          ) : (
+            <Typography variant="body2">Нет данных о лицензии</Typography>
+          )}
+        </CardContent>
+      </Card>
+    </>
+  );
 }
--- src/components/RepositoryList/RepositoryList.tsx
+++ src/components/RepositoryList/RepositoryList.tsx
@@ -8,67 +8,78 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import RepositoryDetails from '../RepositoryDetails/RepositoryDetails';
+import { useState } from 'react';
 
 export default function RepositoryList() {
   const { repositories, isSearchStarted } = useSelector(githubStates);
+  const [selectedRepo, setSelectedRepo] = useState(null);
 
   const formatDate = (isoString: string) => {
     const date = new Date(isoString);
     return date.toLocaleDateString('ru-RU');
   };
 
+  const handleRowClick = repo => {
+    setSelectedRepo(repo);
+  };
+
   return (
     <>
       {isSearchStarted && (
         <section className={styles.section}>
-          <Typography className={styles.subheading} variant="h1" component="h2">
-            Результаты поиска
-          </Typography>
-          <TableContainer>
-            <Table className={styles.tableContainer} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell className={styles.itemTitle}>Название</TableCell>
-                  <TableCell className={styles.itemTitle} align="left">
-                    Язык
-                  </TableCell>
-                  <TableCell className={styles.itemTitle} align="left">
-                    Число&nbsp;форков
-                  </TableCell>
-                  <TableCell className={styles.itemTitle} align="left">
-                    Число&nbsp;звёзд
-                  </TableCell>
-                  <TableCell className={styles.itemTitle} align="left">
-                    Дата&nbsp;обновления
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {repositories.map(repo => (
-                  <TableRow
-                    key={repo.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell className={styles.item} component="th" scope="row">
-                      {repo.name}
+          <div className={styles.container}>
+            <Typography className={styles.subheading} variant="h1" component="h2">
+              Результаты поиска
+            </Typography>
+            <TableContainer>
+              <Table className={styles.tableContainer} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell className={styles.itemTitle}>Название</TableCell>
+                    <TableCell className={styles.itemTitle} align="left">
+                      Язык
                     </TableCell>
-                    <TableCell className={styles.item} align="left">
-                      {repo.language}
+                    <TableCell className={styles.itemTitle} align="left">
+                      Число&nbsp;форков
                     </TableCell>
-                    <TableCell className={styles.item} align="left">
-                      {repo.forks_count}
+                    <TableCell className={styles.itemTitle} align="left">
+                      Число&nbsp;звёзд
                     </TableCell>
-                    <TableCell className={styles.item} align="left">
-                      {repo.stargazers_count}
-                    </TableCell>
-                    <TableCell className={styles.item} align="left">
-                      {formatDate(repo.updated_at)}
+                    <TableCell className={styles.itemTitle} align="left">
+                      Дата&nbsp;обновления
                     </TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {repositories.map(repo => (
+                    <TableRow
+                      key={repo.id}
+                      onClick={() => handleRowClick(repo)}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell className={styles.item} component="th" scope="row">
+                        {repo.name}
+                      </TableCell>
+                      <TableCell className={styles.item} align="left">
+                        {repo.language}
+                      </TableCell>
+                      <TableCell className={styles.item} align="left">
+                        {repo.forks_count}
+                      </TableCell>
+                      <TableCell className={styles.item} align="left">
+                        {repo.stargazers_count}
+                      </TableCell>
+                      <TableCell className={styles.item} align="left">
+                        {formatDate(repo.updated_at)}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+          {selectedRepo && <RepositoryDetails repo={selectedRepo} />}
         </section>
       )}
     </>
