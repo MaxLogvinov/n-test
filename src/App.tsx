b4cb88707f0ab1cb43e81bc0bc629f026fe6335d
Max Logvinov <maxlogvinov93@gmail.com>
2024-09-09 02:26:56 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { Toolbar } from '@mui/material';
 import styles from './App.module.scss';
 import Header from './components/Header/Header';
 import Repository from './components/Repository/Repository';
+import Footer from './components/Footer/Footer';
 
 //ToDo
 
 // Loading
+// filters
 
 export default function App() {
   return (
-    <div className={styles.page}>
-      <Header />
-      <Toolbar />
-      <Repository />
+    <div className={styles.block}>
+      <div className={styles.page}>
+        <Header />
+        <Toolbar />
+        <Repository />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import styles from './Footer.module.scss';
+import { TableFooter } from '@mui/material';
+
+export default function Footer() {
+  return <TableFooter className={styles.footer}></TableFooter>;
+}
